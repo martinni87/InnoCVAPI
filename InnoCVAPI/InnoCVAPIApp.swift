f1d6623660a5ffa6fc9aec4522fec44bcd484f66
martinni87 <30123957+martinni87@users.noreply.github.com>
2022-09-25 21:48:08 +0200

    Initial Commit

---
+++ InnoCVAPI/InnoCVAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnoCVAPIApp.swift
+//  InnoCVAPI
+//
+//  Created by Martin Cordoba on 25/9/22.
+//
+
+import SwiftUI
+
+@main
+struct InnoCVAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
